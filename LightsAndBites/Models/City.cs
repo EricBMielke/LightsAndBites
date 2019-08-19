3e3725293237d7468e184ff2f0d8200e1db98e3e
unknown <firgenscj@gmail.com>
2019-08-19 09:04:21 -0500

    Created User, City, Category and Comment models

---
+++ LightsAndBites/Models/City.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LightsAndBites.Models
+{
+    public class City
+    {
+        [Key]
+        int CityId { get; set; }
+        string CityName { get; set; }
+        string State { get; set; }
+
+    }
+}
---
+++ LightsAndBites/Models/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LightsAndBites.Models
+{
+    public class User
+    {
+        [Key]
+        string Email { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string Hometown { get; set; }
+
+        [ForeignKey("BarId")]
+        int BarCategoryIdOne { get; set; }
+        [ForeignKey("BarId")]
+        int BarCategoryIdTwo { get;set }
+
+        [ForeignKey("RestaurantId")]
+        int RestaurantCategoryIdOne { get; set }
+        [ForeignKey("RestaurantId")]
+        int RestaurantCategoryIdTwo { get; set }
+        [ForeignKey("RestaurantId")]
+        int RestaurantCategoryIdThree { get; set }
+
+        [ForeignKey("EventId")]
+        int EventCategoryIdOne { get; set }
+        [ForeignKey("EventId")]
+        int EventCategoryIdTwo { get; set }
+        [ForeignKey("EventId")]
+        int EventCategoryIdThree { get; set }
+
+        
+    }
+}
