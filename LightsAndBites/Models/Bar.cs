f8ec250f2301c08c6c36985457f0d89c0b338bd2
egilhuber <egilhuber@gmail.com>
2019-08-19 08:55:06 -0500

    created models for bar, event, and restaurant

---
+++ LightsAndBites/Models/Bar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LightsAndBites.Models
+{
+    public class Bar
+    {
+        [Key]
+        public int Id;
+        public string type;
+        [ForeignKey("Category")]
+        public string category;
+        public double longitude;
+        public double latitude;
+        public int likes;
+        public int dislikes;
+        [ForeignKey("CommentId")]
+        public int? commentId;
+        [ForeignKey("CityId")]
+        public int cityId;
+        public string website;
+    }
+}
---
+++ LightsAndBites/Models/Events.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LightsAndBites.Models
+{
+    public class Events
+    {
+        [Key]
+        public int Id;
+        public string type;
+        [ForeignKey("Category")]
+        public string category;
+        public double longitude;
+        public double latitude;
+        [ForeignKey("CommentId")]
+        public int commentId;
+        [ForeignKey("CityId")]
+        public int cityId;
+        public string website;
+
+
+
+    }
+}
