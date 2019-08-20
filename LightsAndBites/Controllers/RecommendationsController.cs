d8cd85054def947bc3f5650fc868816d4f2ca8a5
wjb1114 <kbd1114@gmail.com>
2019-08-20 08:31:00 -0500

    Implemented Controller logic for getting user preferences for all 3 recommendation types. Created private methods that will be used for gathering recommendations to pass into view.

--- LightsAndBites/Controllers/RecommendationsController.cs
+++ LightsAndBites/Controllers/RecommendationsController.cs
@@ -4,16 +4,76 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using LightsAndBites.Models;
+using LightsAndBites.Classes;
+using LightsAndBites.Data;
 
 namespace LightsAndBites.Controllers
 {
     public class RecommendationsController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public RecommendationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         // GET: Recommendations
-        public ActionResult Index()
+        public ActionResult Index(int userId)
+        {
+            UserProfile selectedUser = _context.UserProfile.Where(u => u.Id == userId).Single();
+
+            List<Category> barCategories = new List<Category>();
+            barCategories.Add(_context.Categories.Where(c => c.Id == selectedUser.BarCategoryIdOne).Single());
+            barCategories.Add(_context.Categories.Where(c => c.Id == selectedUser.BarCategoryIdTwo).Single());
+
+            List<Category> eventCategories = new List<Category>();
+            eventCategories.Add(_context.Categories.Where(c => c.Id == selectedUser.EventCategoryIdOne).Single());
+            eventCategories.Add(_context.Categories.Where(c => c.Id == selectedUser.EventCategoryIdTwo).Single());
+            eventCategories.Add(_context.Categories.Where(c => c.Id == selectedUser.EventCategoryIdThree).Single());
+
+            List<Category> restaurantCategories = new List<Category>();
+            restaurantCategories.Add(_context.Categories.Where(c => c.Id == selectedUser.RestaurantCategoryIdOne).Single());
+            restaurantCategories.Add(_context.Categories.Where(c => c.Id == selectedUser.RestaurantCategoryIdTwo).Single());
+            restaurantCategories.Add(_context.Categories.Where(c => c.Id == selectedUser.RestaurantCategoryIdThree).Single());
+
+            List<Recommendation> recommendations = new List<Recommendation>();
+            List<Bar> bars = new List<Bar>();
+            List<Restaurant> restaurants = new List<Restaurant>();
+            List<Events> events = new List<Events>();
+            foreach (Bar b in bars)
+            {
+                recommendations.Add(b);
+            }
+            foreach (Restaurant r in restaurants)
+            {
+                recommendations.Add(r);
+            }
+            foreach (Events e in events)
+            {
+                recommendations.Add(e);
+            }
+            return View();
+        }
+
+        private List<Bar> GetBars(List<Category> categories)
         {
 
-            return View();
+        }
+
+        private List<Restaurant> GetRestaurants(List<Category> categories)
+        {
+
+        }
+
+        private List<Events> GetEvents(List<Category> categories)
+        {
+
+        }
+
+        private List<Recommendation> GetNewGems()
+        {
+
         }
 
         // GET: Recommendations/Details/5
