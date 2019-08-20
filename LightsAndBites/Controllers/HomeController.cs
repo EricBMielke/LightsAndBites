069e4945ea89e612736bc51178656d34cea06442
EricBMielke <51930708+EricBMielke@users.noreply.github.com>
2019-08-20 15:46:23 -0500

    When the user clicks on the contact tab, the event database is now loaded. This is not the end functionality, but is needed for working with the UI

--- LightsAndBites/Controllers/HomeController.cs
+++ LightsAndBites/Controllers/HomeController.cs
@@ -19,6 +19,10 @@ namespace LightsAndBites.Controllers
 {
     public class HomeController : Controller
     {
+        private string token = ApiKey.eventKey;
+        public string Token => token;
+        public string userProfileHometown = "Milwaukee";
+
 
         private ApplicationDbContext _context;
         public HomeController(ApplicationDbContext context)
@@ -40,7 +44,7 @@ namespace LightsAndBites.Controllers
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
-
+            GetAllDailyEvents();
             return View();
         }
 
@@ -53,6 +57,82 @@ namespace LightsAndBites.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public void GetAllDailyEvents()
+        {
+            GetDailyEvents("music");
+            GetDailyEvents("conference");
+            GetDailyEvents("comedy");
+            GetDailyEvents("festivals");
+            GetDailyEvents("family_fun_kids");
+            GetDailyEvents("community");
+            GetDailyEvents("outdoors_recreation");
+            GetDailyEvents("performing_arts");
+        }
+        public void GetDailyEvents(string eventType)
+        {
+            string html = string.Empty;
+            string url = @"http://api.eventful.com/json/events/search?app_key=" + Token + "&category=" + eventType + "&location=" + userProfileHometown + "&sort_order=popularity&page_number=1";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+
+            JObject o = JObject.Parse(html);
+
+            foreach (JObject j in o["events"]["event"])
+            {
+                Events eventt = new Events();
+                eventt.Name = (j["title"]).ToString();
+                eventt.Latitude = Convert.ToDouble((j["latitude"]));
+                eventt.Longitude = Convert.ToDouble((j["longitude"]));
+                eventt.Type = eventType;
+                eventt.StreetAddress = ((j["venue_address"]).ToString());
+                switch (eventType)
+                {
+                    case "music":
+                        eventt.CategoryId = "1";
+                        break;
+                    case "festivals":
+                        eventt.CategoryId = "2";
+                        break;
+                    case "comedy":
+                        eventt.CategoryId = "3";
+                        break;
+                    case "outdoor_recreation":
+                        eventt.CategoryId = "4";
+                        break;
+                    case "performing_arts":
+                        eventt.CategoryId = "8";
+                        break;
+                    case "family_fun_kids":
+                        eventt.CategoryId = "5";
+                        break;
+                    case "community":
+                        eventt.CategoryId = "6";
+                        break;
+                    case "conference":
+                        eventt.CategoryId = "7";
+                        break;
+                }
+                eventt.CityId = 1;
+                if ((j["url"]) == null)    
+                {
+                    eventt.Website = ((j["venue_url"]).ToString());
+                }
+                else
+                {
+                    eventt.Website = ((j["url"]).ToString());
+                }
+                _context.Events.Add(eventt);
+            }
+            _context.SaveChanges();
         }
 
     }
