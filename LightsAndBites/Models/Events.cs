7e46d3ccf09c8c3a58d8c15c6652ab0956b7218f
wjb1114 <kbd1114@gmail.com>
2019-08-20 07:52:16 -0500

    Created new Recommendation class, and set bar, restaurant and event to inherit from it.

--- LightsAndBites/Models/Events.cs
+++ LightsAndBites/Models/Events.cs
@@ -4,10 +4,11 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using LightsAndBites.Classes;
 
 namespace LightsAndBites.Models
 {
-    public class Events
+    public class Events : Recommendation
     {
         [Key]
 
