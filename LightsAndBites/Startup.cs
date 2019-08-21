f6fea7b94394ba68f6e80d7a801ba482b189cf90
wjb1114 <kbd1114@gmail.com>
2019-08-21 10:13:38 -0500

    Updated Recommendation view to use bootstrap, formatting using cards.

--- LightsAndBites/Startup.cs
+++ LightsAndBites/Startup.cs
@@ -35,7 +35,7 @@ namespace LightsAndBites
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            var connection = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Capstone;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            var connection = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=aspnet-LightsAndBites-426A3B2F-0285-414D-826F-158B7C0E3EDD;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
 
             services.AddDbContext<ApplicationDbContext>(options =>
