76c4b6af21d8247ead92fcfd073659a59da20d9d
unknown <firgenscj@gmail.com>
2019-08-22 07:34:53 -0500

    Switched details div to take up whole page

--- LightsAndBites/ConnectionStrings/ConnectionString.cs
+++ LightsAndBites/ConnectionStrings/ConnectionString.cs
@@ -7,6 +7,6 @@ namespace LightsAndBites.ConnectionStrings
 {
     public class ConnectionString
     {
-        public static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=aspnet-LightsAndBites-426A3B2F-0285-414D-826F-158B7C0E3EDD;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Capstone;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
     }
 }
